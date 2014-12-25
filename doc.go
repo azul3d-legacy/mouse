7775fe0f580d62ffdc7f974d1715265b08546c83
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-12-25 10:41:30 -0700

    Move package docs to seperate file.

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2014 The Azul3D Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package mouse implements various mouse related data types.
+package mouse // import "azul3d.org/mouse.v2-dev"
--- mouse.go
+++ mouse.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package mouse implements various mouse related data types.
 package mouse
 
 import (
